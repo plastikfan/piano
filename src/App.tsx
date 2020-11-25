facffacd9df7851511158da7061bd2419a3a7d2e
Plastikfan <plastikfan@outlook.com>
2020-11-25 21:53:33 +0000

    BROKEN: end of step-6 (pg239)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react"
 import { Footer } from "./components/Footer/Footer"
 import { Logo } from "./components/Logo/Logo"
+import { Main } from "./components/Main/main"
+import "./App.css"
 
-function App() {
+const App = () => {
   return (
     <div className="app">
       <Logo />
-      <main className="app-content" />
+      <main className="app-content">
+        <Main />
+      </main>
       <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
