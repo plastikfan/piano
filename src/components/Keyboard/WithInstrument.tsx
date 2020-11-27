2c920c86b2edb2b2417cd9a0f9a02a1ad8d190a3
Plastikfan <plastikfan@outlook.com>
2020-11-27 14:53:03 +0000

    step-8 withInstrument... Ref, Static, Hook

--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,5 +1,5 @@
 
-import React, { FunctionComponent, useEffect } from "react"
+import React, { Component, ComponentType, FunctionComponent, useEffect } from "react"
 import { useAudioContext } from "../AudioContextProvider/useAudioContext"
 
 //pg243
@@ -18,6 +18,132 @@ import "./style.css"
 //
 import { useInstrument } from "../../state/Instrument"
 
+// pg255: Higher Order Components
+//
+import { InstrumentName, Player } from "soundfont-player"
+import { MidiValue } from "../../domain/note"
+import { DEFAULT_INSTRUMENT, AudioNodesRegistry } from "../../domain/sound"
+import { Optional } from "../../domain/types"
+
+import Soundfont from "soundfont-player"
+
+// pg255: Higher Order Components
+//
+interface InjectedProps {
+  loading: boolean
+  play(note: MidiValue): Promise<void>
+  stop(note: MidiValue): Promise<void>
+}
+interface ProviderProps {
+  AudioContext: AudioContextType
+  instrument: InstrumentName
+}
+interface ProviderState {
+  loading: boolean
+  current: Optional<InstrumentName>
+}
+
+// pg256
+//
+export function withInstrument<TProps extends InjectedProps = InjectedProps>(
+  WrappedComponent: ComponentType<TProps>) {
+
+  const displayName = WrappedComponent.displayName || WrappedComponent.name || "Component"
+
+  // this class is equivalent to SoundfontProviderClass (the render props class)
+  // This version is the same as SoundfontProviderClass, except the render method
+  //
+  return class WithInstrument extends Component<ProviderProps, ProviderState> {
+    public static displayName = `withInstrument(${displayName})`
+
+    public static defaultProps = {
+      instrument: DEFAULT_INSTRUMENT
+    }
+
+    constructor(props: ProviderProps) {
+      super(props)
+
+      const { AudioContext } = this.props
+      this.audio = new AudioContext()
+    }
+
+
+    private audio: AudioContext
+    private player: Optional<Player> = null
+    private activeNodes: AudioNodesRegistry = {}
+
+    public state: ProviderState = {
+      loading: false,
+      current: null
+    }
+
+    private resume = async () => {
+      return this.audio.state === "suspended"
+        ? await this.audio.resume()
+        : Promise.resolve()
+    }
+
+    private load = async (instrument: InstrumentName) => {
+      this.setState({ loading: true })
+      this.player = await Soundfont.instrument(this.audio, instrument)
+
+      this.setState({ loading: false, current: instrument })
+    }
+
+    public componentDidMount() {
+      const { instrument } = this.props
+      this.load(instrument)
+    }
+
+    public shouldComponentUpdate({ instrument }: ProviderProps) {
+      return this.state.current !== instrument
+    }
+
+    public componentDidUpdate({
+      instrument: prevInstrument
+    }: ProviderProps) {
+      const { instrument } = this.props
+      if (instrument && instrument !== prevInstrument)
+        this.load(instrument)
+    }
+
+    public play = async (note: MidiValue) => {
+      await this.resume()
+      if (!this.player) return
+
+      const node = this.player.play(note.toString())
+      this.activeNodes = { ...this.activeNodes, [note]: node }
+    }
+
+    public stop = async (note: MidiValue) => {
+      await this.resume()
+      if (!this.activeNodes[note]) return
+
+      this.activeNodes[note]!.stop()
+      this.activeNodes = { ...this.activeNodes, [note]: null }
+    }
+
+    // loading, play and stop are items that are relevent to Keyboard and hence
+    // these need to be injected the wrapped component via its props
+    //
+    public render() {
+      const injected = {
+        loading: this.state.loading,
+        play: this.play,
+        stop: this.stop
+      } as InjectedProps
+      return (
+        <WrappedComponent {...this.props} {...(injected as TProps)} />
+      )
+    }
+  }
+}
+
+//pg258
+// WrappedKeyboard is a variable that contains JSX
+//
+const WrappedKeyboard = withInstrument(Keyboard)
+
 
 // pg243
 //
@@ -26,11 +152,20 @@ export const KeyboardWithInstrument: FunctionComponent = () => {
   const { instrument } = useInstrument()
 
   return (
-    <SoundfontProvider
+    // pg258: HOC VERSION using WrappedKeyboard function
+    //
+    <WrappedKeyboard
       AudioContext={AudioContext}
       instrument={instrument}
-      render={(props) => <Keyboard {...props} />}
     />
+
+    // RENDER PROPS VERSION:
+    //
+    // <SoundfontProvider
+    //   AudioContext={AudioContext}
+    //   instrument={instrument}
+    //   render={(props) => <Keyboard {...props} />}
+    // />
   )
 }
 
