186db983784e0647836a417c7d972733d52d738b
Plastikfan <plastikfan@outlook.com>
2020-11-27 11:13:20 +0000

    (step-7): Render props by class component and functional component

---
+++ src/adapters/Soundfont/SoundfontProvider.ts
@@ -0,0 +1,86 @@
+import { FunctionComponent, ReactElement, useCallback, useEffect, useRef, useState } from "react";
+import Soundfont, { InstrumentName, Player } from "soundfont-player";
+import { MidiValue } from "../../domain/note";
+import { AudioNodesRegistry, DEFAULT_INSTRUMENT } from "../../domain/sound";
+import { Optional } from "../../domain/types";
+
+interface ProviderProps {
+  instrument?: InstrumentName
+  AudioContext: AudioContextType
+  render(props: ProvidedProps): ReactElement
+}
+
+interface ProvidedProps {
+  loading: boolean
+  play(note: MidiValue): Promise<void>
+  stop(note: MidiValue): Promise<void>
+}
+
+export const SoundfontProvider: FunctionComponent<ProviderProps> = ({
+  AudioContext,
+  instrument,
+  render
+}) => {
+  let activeNodes: AudioNodesRegistry = {}
+
+  const [current, setCurrent] = useState<Optional<InstrumentName>>(
+    null
+  )
+  const [loading, setLoading] = useState<boolean>(false)
+  const [player, setPlayer] = useState<Optional<Player>>(null)
+  const audio = useRef(new AudioContext())
+
+  const loadInstrument = useCallback(() => load(instrument), [
+    instrument
+  ])
+
+  // This is new as described on pg242
+  //
+  useEffect(() => {
+    if (!loading && instrument !== current) loadInstrument()
+  }, [loadInstrument, loading, instrument, current])
+
+  async function resume() {
+    return audio.current.state === "suspended"
+      ? await audio.current.resume()
+      : Promise.resolve()
+  }
+
+  async function load(
+    instrument: InstrumentName = DEFAULT_INSTRUMENT
+  ) {
+    setLoading(true)
+    const player = await Soundfont.instrument(
+      audio.current,
+      instrument
+    )
+
+    setLoading(false)
+    setCurrent(instrument)
+    setPlayer(player)
+  }
+
+  async function play(note: MidiValue) {
+    await resume()
+    if (!player) return
+
+    const node = player.play(note.toString())
+    activeNodes = { ...activeNodes, [note]: node }
+  }
+
+  async function stop(note: MidiValue) {
+    await resume()
+    if (!activeNodes[note]) return
+
+    activeNodes[note]!.stop()
+    activeNodes = { ...activeNodes, [note]: null }
+  }
+
+  // return statement as described on pg243
+  //
+  return render({
+    loading,
+    play,
+    stop
+  })
+}
--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,8 +1,15 @@
 
 import React, { FunctionComponent, useEffect } from "react"
 import { useAudioContext } from "../AudioContextProvider/useAudioContext"
-import { useSoundfont } from "../../adapters/Soundfont/useSoundfont"
-import { useMount } from "../../utils/useMount"
+
+//pg243
+//
+import { SoundfontProvider } from "../../adapters/Soundfont"
+
+// pg243, no longer require these imports
+//
+// import { useSoundfont } from "../../adapters/Soundfont/useSoundfont"
+// import { useMount } from "../../utils/useMount"
 import { Keyboard } from "../Keyboard/Keyboard"
 
 import "./style.css"
@@ -11,24 +18,43 @@ import "./style.css"
 //
 import { useInstrument } from "../../state/Instrument"
 
-// This file new with pg229 (step-4)
+
+// pg243
 //
 export const KeyboardWithInstrument: FunctionComponent = () => {
   const AudioContext = useAudioContext()!
-
-  // pg239: useInstrument
-  //
   const { instrument } = useInstrument()
-  const { loading, current, play, stop, load } = useSoundfont({ AudioContext })
 
-  // we replace "useMount(load)" with useEffect as below pg239:
-  // "Notice that we replace useMount() hook with useEffect() hook. We have to do that
-  // since we want to dynamically change our instruments sounds set, instead of loading
-  // it once when mounted"
-  //
-  useEffect(() => {
-    if (!loading && instrument !== current) load(instrument)
-  }, [load, loading, current, instrument])
-
-  return <Keyboard loading={loading} play={play} stop={stop} />
+  return (
+    <SoundfontProvider
+      AudioContext={AudioContext}
+      instrument={instrument}
+      render={(props) => <Keyboard {...props} />}
+    />
+  )
 }
+
+// pg243, use SoundfontProvider, so this version of KeyboardWithInstrument
+// is redundant
+//
+// This file new with pg229 (step-4)
+//
+// export const KeyboardWithInstrument: FunctionComponent = () => {
+//   const AudioContext = useAudioContext()!
+
+//   // pg239: useInstrument
+//   //
+//   const { instrument } = useInstrument()
+//   const { loading, current, play, stop, load } = useSoundfont({ AudioContext })
+
+//   // we replace "useMount(load)" with useEffect as below pg239:
+//   // "Notice that we replace useMount() hook with useEffect() hook. We have to do that
+//   // since we want to dynamically change our instruments sounds set, instead of loading
+//   // it once when mounted"
+//   //
+//   useEffect(() => {
+//     if (!loading && instrument !== current) load(instrument)
+//   }, [load, loading, current, instrument])
+
+//   return <Keyboard loading={loading} play={play} stop={stop} />
+// }
